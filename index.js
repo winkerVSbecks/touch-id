9927d852ecca04b4eddd0c2446ec771a9aefa821
Varun Vachhar <varunvachhar@gmail.com>
2016-05-23 10:41:46 -0400

    new ⏲

--- index.js
+++ index.js
@@ -2,23 +2,33 @@ window.onload = onLoad;
 
 const easeInOutQuint = 'cubic-bezier(0.86, 0, 0.07, 1)';
 const easeInQuart = 'cubic-bezier(0.895, 0.03, 0.685, 0.22)';
-const timing = {
-  duration: 4000,
-  iterations: Infinity,
-};
+const timing = { duration: 4000 };
 
 function onLoad() {
   const dashes = [150, 330, 370, 410, 445, 470, 485, 490, 425];
 
-  dashes.forEach((dash, idx) => {
+  const loopBirthAnims = dashes.map((dash, idx) => {
     const loop = document.querySelector(`#loop${ idx }`);
     const keyframes = generateLoopKeyframes(0, dash);
-    loop.animate(keyframes, timing);
+    return new KeyframeEffect(loop, keyframes, {
+      duration: 8000,
+      delay: idx * 100,
+    });
   });
+
+  var loopBirthGroup = new GroupEffect(loopBirthAnims);
+
+  var sequence = new GroupEffect([
+    loopBirthGroup,
+    // circle animation goes here
+  ]);
+
+  (function play() {
+    return document.timeline.play(sequence).onfinish = play;
+  })();
 }
 
 function generateLoopKeyframes(idx, Dashoffset) {
-  const i = idx / 100;
   return [{
     strokeDashoffset: 0,
     opacity: 0,
@@ -27,30 +37,24 @@ function generateLoopKeyframes(idx, Dashoffset) {
   }, {
     strokeDashoffset: 0,
     opacity: 0,
-    offset: 0.01 + i,
+    offset: 0.01,
     easing: easeInOutQuint
   }, {
     opacity: 1,
-    offset: 0.02 + i,
+    offset: 0.02,
     easing: easeInOutQuint
   }, {
     strokeDashoffset: Dashoffset,
-    offset: 0.25 + i,
+    offset: 0.25,
     easing: easeInOutQuint
   }, {
     strokeDashoffset: Dashoffset,
+    offset: 0.875,
+    easing: easeInQuart,
+  }, {
     opacity: 1,
-    offset: 0.5,
+    offset: 0.99,
     easing: easeInQuart
-  }, {
-    opacity: 1,
-    offset: 0.74,
-    easing: easeInQuart,
-  }, {
-    strokeDashoffset: 0,
-    opacity: 0,
-    offset: 0.75 + i,
-    easing: easeInQuart,
   }, {
     strokeDashoffset: 0,
     opacity: 0,
